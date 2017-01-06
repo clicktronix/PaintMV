08715b5b1e99f6c5721bf01af0a822134f643ddc
Vladislav Manakov <s28.morgan@gmail.com>
2017-01-07 00:06:16 +0700

    Added cut feature

---
+++ Paint/Shapes/ShapesList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PaintMV.Shapes
+{
+    [Serializable]
+    public class ShapesList
+    {
+        public List<Shape> AllShapes = new List<Shape>();
+    }
+}
